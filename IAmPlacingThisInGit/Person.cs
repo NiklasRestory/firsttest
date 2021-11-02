08a19dbfc43eaf24741c292b37f43b6493f57c85
Niklas Cullberg <niklas.cullberg@restory.ai>
2021-11-02 03:46:14 +0100

    Program created.
    Created Program and Person class. Added random (4) functionality to program, gave person age and name variables and a constructor that added these.

---
+++ IAmPlacingThisInGit/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IAmPlacingThisInGit
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
---
+++ IAmPlacingThisInGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IAmPlacingThisInGit
+{
+    class Program
+    {
+        static int randomNumber = 4;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Number: " + RandomNumber());
+        }
+
+        static int RandomNumber()
+        {
+            return randomNumber;
+        }
+    }
+}
